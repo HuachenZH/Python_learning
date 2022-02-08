25c9e814bf3c7112205a30228112453bce4680dc
Huachen ZHANG <91951021+HuachenZH@users.noreply.github.com>
2022-02-08 12:30:07 +0100

    Update openFiles.py

--- Chapter7_files/openFiles.py
+++ Chapter7_files/openFiles.py
@@ -28,3 +28,20 @@ except:
 for line in fhand:
     print(line)
 
+
+"""
+Exercise : 
+Write a program to prompt for a file name, and then read through the file and look for lines of the form:
+X-DSPAM-Confidence:0.8475
+When you encounter a line that starts with "X-DSPAM-Confidence:" pull apart the line to extract the floating-point number on the line. Count these lines and then compute the total of the spam confidence values from these lines. When you reach the end of the file, print out the average spam confidence.
+"""
+fhand=open('mbox-short.txt')
+total=0
+count=0
+for line in fhand:
+    if line.startswith('X-DSPAM-Confidence:'):
+        index=line.find(':')
+        total=total+float(line[index+1:])
+        count=count+1
+average=total/count
+print('Average spam confidence : ',average)
