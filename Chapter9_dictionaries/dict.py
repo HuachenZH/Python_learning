7e28b33dd9c9e8b61036f644d9fc2106f0e58467
Huachen ZHANG <91951021+HuachenZH@users.noreply.github.com>
2022-02-11 15:13:57 +0100

    Update dict.py

--- Chapter9_dictionaries/dict.py
+++ Chapter9_dictionaries/dict.py
@@ -1,3 +1,7 @@
+# =============================================================================
+# most basic
+# =============================================================================
+
 # to create a dictionary :
 price=dict()
 # or
@@ -7,6 +11,10 @@ price={}
 # add elements
 price['apple']=2
 price['banana']=3
+
+# =============================================================================
+# .get()
+# =============================================================================
 
 # to get an element which exists in the dictionary (or not):
 x=price.get('bana',0) # if 'bana' doesn't exist in the dictionary, then x equals to 0
@@ -19,4 +27,20 @@ for formats in liste:
     res[formats]=res.get(formats,0)+1
 
 
-
+# =============================================================================
+# inspect a dictionary
+# =============================================================================
+# print the keys of a dictionary
+print('print the keys of a dictionary')
+print(list(res))
+print(res.keys())
+# print the values of a dictionary
+print('print the values of a dictionary')
+print(res.values())
+# print both keys and values
+print('print both keys and values')
+print(res)
+print(res.items())
+    
+    
+    
