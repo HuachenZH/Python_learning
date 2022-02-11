2f27f7480fddd051568585ef58e19a59a52a269e
Huachen ZHANG <91951021+HuachenZH@users.noreply.github.com>
2022-02-11 14:49:42 +0100

    Update dict.py

--- Chapter9_dictionaries/dict.py
+++ Chapter9_dictionaries/dict.py
@@ -1,1 +1,14 @@
+# to create a dictionary :
+price=dict()
+# or
+price={}
+# just like how we create lists
+
+# add elements
+price['apple']=2
+price['banana']=3
+
+# to get an element which exists in the dictionary (or not):
+x=price.get('bana',0) # if 'bana' doesn't exist in the dictionary, then x equals to 0
+
 
