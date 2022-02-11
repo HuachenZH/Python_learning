d72413c9f7fc425acdad6895798e30d34a502fde
Huachen ZHANG <91951021+HuachenZH@users.noreply.github.com>
2022-02-11 15:00:52 +0100

    Update dict.py

--- Chapter9_dictionaries/dict.py
+++ Chapter9_dictionaries/dict.py
@@ -11,4 +11,12 @@ price['banana']=3
 # to get an element which exists in the dictionary (or not):
 x=price.get('bana',0) # if 'bana' doesn't exist in the dictionary, then x equals to 0
 
+# an application of .get()
+#counting number in a list, put the results in a dictionary
+res=dict()
+liste=['txt','pdf','ai','pdf','docx','m','txt','xlsm','R','R','py']
+for formats in liste:
+    res[formats]=res.get(formats,0)+1
+
+
 
