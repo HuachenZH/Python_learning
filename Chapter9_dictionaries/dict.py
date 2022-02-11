cb2f691d53c9ace71bd98879db945cffe0c7c90e
Huachen ZHANG <91951021+HuachenZH@users.noreply.github.com>
2022-02-11 16:07:31 +0100

    Update dict.py

--- Chapter9_dictionaries/dict.py
+++ Chapter9_dictionaries/dict.py
@@ -51,6 +51,36 @@ for fruit,price in prices.items():
     print(fruit,' ',price)
 
 
+# =============================================================================
+# An exercise for fun, find which word appears the most frequently in a text
+# =============================================================================
+# The basic logic is, firstly read the text file, split it into words, then put the words into a dictionary,
+# then find the max in the dictionary
+hist=dict()
+fhand=open('romeo.txt')
+# put it in a dictionary
+for line in fhand:
+    words=line.split()
+    for word in words:
+        hist[word]=hist.get(word,0)+1
+# inspect the dictionary
+for keys,values in hist.items():
+    print(keys,'\t\t',values)
+
+print('\n')
+print('-------------------')
+print('\n')
+# find the word which appears the most frequently
+maxWord=None
+maxCount=0
+for word,count in hist.items():
+    if maxCount<count:
+        maxWord=word
+        maxCount=count
+# in case there are several max
+for word,count in hist.items():
+    if count==maxCount:
+        print(word,'\t',count)
 
 
 
