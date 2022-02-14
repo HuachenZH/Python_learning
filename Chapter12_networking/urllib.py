ceb5987e5257cdb78bc5144f61e486ff204eedb1
Huachen ZHANG <91951021+HuachenZH@users.noreply.github.com>
2022-02-14 16:16:19 +0100

    Create urllib.py

---
+++ Chapter12_networking/urllib.py
@@ -0,0 +1,9 @@
+# urllib : URL library
+
+# Example code 
+import urllib.request
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt') # comparing to the precedent code in socket.py, this line has done the GET, done the .encode()
+for line in fhand:
+    print(line.decode().strip())
+
+
