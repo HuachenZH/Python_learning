ccd13e194202edf7baf1a17e4634dd8a01a06d22
Huachen ZHANG <91951021+HuachenZH@users.noreply.github.com>
2022-02-15 18:04:44 +0100

    Update urllib.py

--- Chapter12_networking/urllib.py
+++ Chapter12_networking/urllib.py
@@ -5,5 +5,14 @@ import urllib.request
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt') # comparing to the precedent code in sockets.py, this line has done the GET, done the .encode()
 for line in fhand:
     print(line.decode().strip())
+#
+
+
+
+
+
+
+
+
 
 
