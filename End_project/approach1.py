1df447d69dc43b45f689d7d0c6ff060711af133e
Huachen ZHANG <91951021+HuachenZH@users.noreply.github.com>
2022-03-02 10:05:38 +0100

    Update approach1.py

--- End_project/approach1.py
+++ End_project/approach1.py
@@ -7,4 +7,7 @@ This is a temporary script file.
 # There exists different approaches to print..
 
 # Approach1, "matrix" way.
-# A big "matrix", or in Python words, list.
+# A big "matrix", or in Python words, list. An arithmetic format is an element of the list, the element can be a string or a tuple.
+# the difficulty is to print them in a vertical way.
+
+# So test the feasibility of this approach.
