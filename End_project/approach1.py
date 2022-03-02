d524459ef645e1c9c4b5d80fffede8fb282c31ea
Huachen ZHANG <91951021+HuachenZH@users.noreply.github.com>
2022-03-02 09:49:58 +0100

    Create approach1.py

---
+++ End_project/approach1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+This is a temporary script file.
+"""
+
+# There exists different approaches to print..
+
+# Approach1, "matrix" way.
+# A big "matrix", or in Python words, list.
