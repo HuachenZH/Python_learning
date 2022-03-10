5871061387ca7cacde254abd502b79eef6926718
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-03-10 15:34:55 +0100

    added: constructor, deposit

--- Project_budget_app/budget.py
+++ Project_budget_app/budget.py
@@ -1,7 +1,16 @@
 class Category:
-
-
-
-
-
+	ledger=[] #instance variable=attribut. It's a list  
+	name='' # name of the category
+	def __init__(self,name): # constructor
+		self.name=name
+		self.ledger=[]
+	def deposit(self,amount, *description):
+		if not (isinstance(amount, int) or isinstance(amount, float)):
+			print('Amount must be a number')
+			return
+		if description==None:
+			description=''
+		self.ledger.append({"amount":amount,"description":description})
+		
+		
 def create_spend_chart(categories):
