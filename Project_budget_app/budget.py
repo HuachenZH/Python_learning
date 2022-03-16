99fa2070b30d38523c5503a3746727c562424dc2
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-03-16 21:53:39 +0100

    bug fixed .withdraw()
    Two changes.
    1) in the last version, i called the deposit method to modify ledger. Now i found it's not so convenient, so i append the ledger by hand.
    2) a new if else, the same issue with tuple of *description.

--- Project_budget_app/budget.py
+++ Project_budget_app/budget.py
@@ -1,14 +1,14 @@
 class Category:
-	ledger=[] #instance variable=attribut. It's a list  
-	name='' # name of the category
-	def __init__(self,name): # constructor
-		self.name=name
-		self.ledger=[]
-	def deposit(self,amount, *description):
-		if not (isinstance(amount, int) or isinstance(amount, float)):
-			print('Amount must be a number')
-			return
-		if description==(): # when there is no argument description, desciption is not None but (), an empty tuple... creepy
+    ledger=[] #instance variable=attribut. It's a list  
+    name='' # name of the category
+    def __init__(self,name): # constructor
+        self.name=name
+        self.ledger=[]
+    def deposit(self,amount, *description):
+        if not (isinstance(amount, int) or isinstance(amount, float)):
+            print('Amount must be a number')
+            return
+        if description==(): # when there is no argument description, desciption is not None but (), an empty tuple... creepy
             description=''
         else:
             description=description[0] # i don't know why but description is a tuple here. because of the *. Without *, it's a string
@@ -22,9 +22,13 @@ class Category:
         if self.get_balance()-amount<0:
             return False
         else:
+            if description==(): 
+                description=''
+            else:
+                description=description[0]
             #the amount should be stored in the ledger as a negative number
             amount=-1*amount
-            self.deposit(amount,description)
+            self.ledger.append({"amount":amount,"description":description})
             return True
 	def transfer(self,amount,cat): # cat is the destinated category
     # self will loss money, cat will get money
