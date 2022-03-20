2721d816c294fe6c7f0ff7d46e5aeb02dcbfd2d4
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-03-20 16:31:07 +0100

    Built create_spend_chart
    All the unit tests ok.
    Project finished.

--- Project_budget_app/budget.py
+++ Project_budget_app/budget.py
@@ -5,7 +5,7 @@ Created on Fri Mar 11 19:29:10 2022
 @author: e^(j·2pi)
 """
 import math
-
+from arith import cbind, rbind, tstring
 class Category:
     ledger=[] #instance variable=attribut. It's a list  
     name='' # name of the category
@@ -152,20 +152,92 @@ class Category:
         out+='Total: '
         # calculate the total
         out+=format(self.get_balance(),'.2f')
-
         return out
     
+def create_spend_chart(listcategories):
+    '''
+    Chart showing the percentage spent in each category
+
+    Parameters
+    ----------
+    listcategories : TYPE list 
+        A list of of categories.
+
+    Returns
+    -------
+    A long string...
+
+    '''
+    # 1. get the percentage
+    # 1.1 create a dictionary which holds values
+    dictper={} #a dict of percentage
+    for i in listcategories: # i will be a category like food, drink...
+        dictper[i.name]=0
+        for j in i.ledger: # j is a dictionary like {"amount":500,"description:":"depo"}
+            if j["amount"]<0: # if it's withdraw
+                dictper[i.name]+=j["amount"] # add to the dictionary
+    # 1.2 calculate the total sum
+    # dictper is like: {'Business': -10.99, 'Food': -105.55, 'Entertainment': -33.4}
+    somme=sum(i for i in dictper.values()) # the total sum of spent
+    # 1.3 calculate percentage
+    for i in dictper:
+        dictper[i]=math.floor(dictper[i]/somme*100/10)*10
+        #                 |                         |---|
+        #                 |                           |
+        #            round down                 to the nearest 10
+    # 2. print the chart
+    out=''
+    # i cut the chart into three parts, title, top and bot
+    # title is 'Percentage spent by category'
+    # top is the y axis and lots of 'o', and the    -----------
+    # bot is the x axis (name of category)
+    # --------- 
+    # 2.1 title
+    title='Percentage spent by category'
+    # 2.2 top    
+    # 2.2.1 y axis
+    col1='100\n 90\n 80\n 70\n 60\n 50\n 40\n 30\n 20\n 10\n  0'
+    col2='|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|'
+    top=cbind(col1,col2)
+    # 2.2.2 lots of 'o'
+    tmp=''
+    for v in dictper.values():
+        tmp=''
+        for i in range(int(10-v/10)): # print whitespaces
+            tmp+='   \n' # 3 whitespaces
+        for i in range(int(v/10+1)): # print o
+            tmp+=' o \n'
+        tmp=tmp[0:len(tmp)-1] # enlever le dernier \n
+        top=cbind(top,tmp)
+    top=cbind(top,' \n \n \n \n \n \n \n \n \n \n ') # the rightest column of whitespaces 
+    # 2.2.3 the ---------- line
+    line='    ' # always starts by four whitespaces
+    for i in range(len(listcategories)):
+        line+='---'
+    line+='-'
+    # paste line and chart
+    top=rbind(top,line)
+    # 2.3 bot
+    # 2.3.1 the leftmost rectangle of whitespace
+    height=max(len(k) for k in dictper.keys()) # the height of the bot
+    bot=''
+    for i in range(height):
+        bot+='    \n' # always four whitespaces
+    bot=bot[0:len(bot)-1] # enlever le dernier \n
+    # 2.3.2 the x axis, name in vertical
+    for k in dictper.keys():
+        for j in range(height-len(k)):
+            k+=' '
+        bot=cbind(bot,tstring(k))
+    # 2.3.3 the rightmost column of whitespace
+    tmp=''
+    for i in range(height):
+        tmp+=' \n'
+    tmp=tmp[0:len(tmp)-1] # enlever le dernier \n
+    bot=cbind(bot,tmp)
+    # 2.4 paste together
+    out=rbind(title,top)
+    out=rbind(out,bot)
+    return out
 
 
-
-def create_spend_chart(categories):
-
-
-
-
-    
-    
-    
-    
-    
-    
