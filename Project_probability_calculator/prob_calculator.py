4245c330b5b16ca4861156cca55a4351c6f36f54
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-03-27 22:30:20 +0200

    built __init__ (!!)
    learnt: five types of arguments when defining function. 
    Here I used arbitrary keyword argument, **args.
    (In another project, i used *args, it's arbitrary positional argument)

--- Project_probability_calculator/prob_calculator.py
+++ Project_probability_calculator/prob_calculator.py
@@ -3,6 +3,14 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-
+    hat=dict()
+    contents=list()
+    def __init__(self,**args):
+        # print(type(args)) # the type of args is dict
+        # print(args) # returns {'yellow': 3, 'blue': 2, 'green': 6} (for example)
+        hat=args
+        for k,v in args.items():
+            for i in range(v):
+                self.contents.append(k)
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
