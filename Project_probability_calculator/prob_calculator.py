7c2ea0d93b88ec080da5bdcecfe35c2879d7d408
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-03-27 22:51:07 +0200

    built __str__ and bug fixed (!!)
    bug fixed in __init__,
    it's not hat=args but should be self.hat=args
    
    What i want is just to print self.hat, however i cannot return self.hat, as it's a dict but not a str. So i wrote this stupid method...

--- Project_probability_calculator/prob_calculator.py
+++ Project_probability_calculator/prob_calculator.py
@@ -8,9 +8,15 @@ class Hat:
     def __init__(self,**args):
         # print(type(args)) # the type of args is dict
         # print(args) # returns {'yellow': 3, 'blue': 2, 'green': 6} (for example)
-        hat=args
+        self.hat=args
         for k,v in args.items():
             for i in range(v):
                 self.contents.append(k)
-
+    def __str__(self): # i think it's useless to build this method. However i fixed a bug while building it
+        out='{'
+        for k,v in self.hat.items():
+            out+="'"+k+"'"+': '+"'"+str(v)+"'"+', '
+        out+='}'
+        return out
+    
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
