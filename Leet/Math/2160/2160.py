141cc14362f69a1ecde61baef24d5b4a9127bb6b
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-24 00:30:14 +0200

    Update 2160.py
    get the four digits
    next : find all the combinations

--- Leet/Math/2160/2160.py
+++ Leet/Math/2160/2160.py
@@ -1,2 +1,21 @@
+# paths:
+    # get the four digits
+    # combination
+    # find the minimum
+
+
 class Solution:
     def minimumSum(self, num: int) -> int:
+        # get the four digits
+        digit1=num%10
+        digit2=int(num%100/10) # int() can round down
+        digit3=int(num%1000/100)
+        digit4=int(num%10000/1000)
+        
+        # combination
+        return [digit1, digit2, digit3, digit4]
+    
+
+
+sol=Solution()
+sol.minimumSum(2887)
