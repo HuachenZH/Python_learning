ed3b348a1b8963440f1e0aa3716ba86b30ed6a31
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-09-23 09:46:51 +0200

    Update fct.py

--- Chapter?_function/fct.py
+++ Chapter?_function/fct.py
@@ -1,4 +1,14 @@
+# ******************* default value of a fct *************************
 def iden(prenom, nom, pays="France"):
   pass
 # if the third argument pays is not given, its value will automatically be "France"
 
+# ******************** global variable in a function **************************
+x=2
+def rien():
+  global x
+  x=5
+  porint(x)
+print(x) # gives 2
+rien() # gives 5
+print(x) # gives 5
